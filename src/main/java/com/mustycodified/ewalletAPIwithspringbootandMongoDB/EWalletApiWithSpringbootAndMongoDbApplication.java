e7ac0b7a0d0fff00c5f65532c6a7c70f8e793452
Mustapha Musa <mustapha.musa@decagon.dev>
2023-10-09 16:24:34 +0100

    Initial commit

---
+++ src/main/java/com/mustycodified/ewalletAPIwithspringbootandMongoDB/EWalletApiWithSpringbootAndMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.mustycodified.ewalletAPIwithspringbootandMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EWalletApiWithSpringbootAndMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EWalletApiWithSpringbootAndMongoDbApplication.class, args);
+	}
+
+}
