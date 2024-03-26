34534f3c39c458bbb005315fe22889bc147ef18f
Mustapha Musa <mustapha.musa@decagon.dev>
2024-03-26 22:39:05 +0100

    changes

--- src/main/java/com/mustycodified/ewalletAPIwithspringbootandMongoDB/EWalletApiWithSpringbootAndMongoDbApplication.java
+++ src/main/java/com/mustycodified/ewalletAPIwithspringbootandMongoDB/EWalletApiWithSpringbootAndMongoDbApplication.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpHeaders;
@@ -22,6 +23,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @OpenAPIDefinition(
 		info=@Info(
@@ -61,7 +64,9 @@ public class EWalletApiWithSpringbootAndMongoDbApplication implements CommandLin
 	private final MongoTemplate mongoTemplate;
 
 	public static void main(String[] args) {
-		SpringApplication.run(EWalletApiWithSpringbootAndMongoDbApplication.class, args);
+		ConfigurableApplicationContext ctx =
+				SpringApplication.run(EWalletApiWithSpringbootAndMongoDbApplication.class, args);
+		       Arrays.stream(ctx.getBeanDefinitionNames()).forEach(System.out::println);
 	}
 
 	@Bean
@@ -92,5 +97,7 @@ public class EWalletApiWithSpringbootAndMongoDbApplication implements CommandLin
 //		mongoTemplate.dropCollection("wallets");
 //		mongoTemplate.dropCollection("users");
 	}
+
+
 
 }
